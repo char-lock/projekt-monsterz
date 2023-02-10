842749aad14331ec602f4cca72f44733c699c39d
Lockett, Charlotte F <103963818+char-lock@users.noreply.github.com>
2023-02-10 14:04:06 -0500

    Fix express response; add page design

--- api/src/app.ts
+++ api/src/app.ts
@@ -8,7 +8,9 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => { app.render('200 OK'); });
+app.get('/', (req, res) => {
+    res.json({ "body": "200 OK" });
+});
 
 app.listen(process.env.API_PORT || 8080, () => {
     return console.log(`Express is listening at http://localhost:${process.env.API_PORT || 8080}`);
