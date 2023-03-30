ab31530f0e10bb9f94356c851399a1d05010811e
Jesse Tortorella <72392491+Jtortorella@users.noreply.github.com>
2023-03-30 18:44:29 -0400

    Updated UI.

--- ui/src/app/dashboard/dashboard.component.ts
+++ ui/src/app/dashboard/dashboard.component.ts
@@ -1,5 +1,5 @@
 import { AccountService } from '../services/account-service.service';
-import { Component, ElementRef, OnInit} from '@angular/core';
+import { Component, ElementRef, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,62 +7,65 @@ import { Component, ElementRef, OnInit} from '@angular/core';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
-  globalLeaderBoardSelected = false;
+  globalLeaderBoardSelected = true;
   globalLeaderBoard = [
     ["Earl", 200],
     ["PanPeter", 500],
     ["Grimjow", 5000],
     ["Dumbo", 6000]
   ];
-   friendsOrClassLeaderBoard = [
+  friendsOrClassLeaderBoard = [
     ["Joe", 200],
     ["Bojack", 500],
     ["PixieTawk", 5000],
     ["Sleepy", 6000]
-   ];
+  ];
   progress: number = 15;
   score: number = 200;
   CurrentLesson: string = "Lesson 1-8";
   constructor(private accountService: AccountService
     // private leaderBoardService: LeaderBoardService
-    ){}
+  ) { }
   ngOnInit() {
     // this.globalLeaderBoard = leaderBoardService.getGlobalLeaderboard();
     // this.friendsOrClassLeaderBoard = leaderBoardService.getFriendLeaderboard();
-    this.globalLeaderBoard =this.globalLeaderBoard.sort(this.sortArrayByScore);
-    this.friendsOrClassLeaderBoard =this.friendsOrClassLeaderBoard.sort(this.sortArrayByScore);
+    this.globalLeaderBoard = this.globalLeaderBoard.sort(this.sortArrayByScore);
+    this.friendsOrClassLeaderBoard = this.friendsOrClassLeaderBoard.sort(this.sortArrayByScore);
   }
   leaderBoard: (string | number)[][] = this.friendsOrClassLeaderBoard;
 
-    
-    setProgressBar() {
-      let styles = {
-        // 'background': 'linear-gradient(90deg, green 15%, black 85%)',
-        'background': 
-        'linear-gradient(90deg, #4dd700 ' + 0 + '% ' + (this.progress) + '%, #86a9b0 ' + (this.progress) + '% 100%)'        
-      }
-        return styles;
+  
+
+  setProgressBar() {
+    let styles = {
+      // 'background': 'linear-gradient(90deg, green 15%, black 85%)',
+      'background':
+        'linear-gradient(95deg, #4dd700 ' + 0 + '% ' + (this.progress) + '%, #86a9b0 ' + (this.progress) + '% 100%)'
     }
-    onToggleButtonClick() {
-      this.globalLeaderBoardSelected = !this.globalLeaderBoardSelected;
-      if (this.globalLeaderBoardSelected) {
-        this.leaderBoard = this.globalLeaderBoard;
-      }
-      else {
+    return styles;
+  }
+  onToggleButtonClick() {
+    this.globalLeaderBoardSelected = !this.globalLeaderBoardSelected;
+    if (this.globalLeaderBoardSelected) {
+      this.leaderBoard = this.globalLeaderBoard;
+    }
+    else {
       this.leaderBoard = this.friendsOrClassLeaderBoard;
-      }
-    }
-    sortArrayByScore(a: (string|number)[], b: (string|number)[]) {
-        if (a[1] === b[1]) {
-          return 0;
-        }
-        else {
-          if (a[1] < b[1]) {
-            return 1;
-          }
-          else {
-            return -1;
-          }
-        }
     }
   }
+  sortArrayByScore(a: (string | number)[], b: (string | number)[]) {
+    if (a[1] === b[1]) {
+      return 0;
+    }
+    else {
+      if (a[1] < b[1]) {
+        return 1;
+      }
+      else {
+        return -1;
+      }
+    }
+  }
+
+
+}
