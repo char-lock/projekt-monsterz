0f8e6bfe468c48bba0b0f36fd08ad91e77290688
Jesse Tortorella <72392491+Jtortorella@users.noreply.github.com>
2023-04-24 11:14:57 -0400

    Updated some components. Attempted to create toast message. Need to find best practices for deleting nodes.

---
+++ ui/src/app/directives/message-color.directive.ts
@@ -0,0 +1,12 @@
+// import { Directive, ElementRef } from "@angular/core";
+
+
+// @Directive({
+//      selector: '[messageColor]'
+// })
+// export class MessageColor {
+
+//   constructor(private element: ElementRef) {
+//      this.element.nativeElement
+//   }
+// }
---
+++ ui/src/app/pages/success-message/success-message.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-success-message',
+  templateUrl: './success-message.component.html',
+  styleUrls: ['./success-message.component.css']
+})
+export class SuccessMessageComponent {
+}
