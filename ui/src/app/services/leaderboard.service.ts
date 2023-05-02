c762f7e3028e5f92c55ef20526d8ace31831664a
Lockett, Charlotte F <lockettc@protonmail.com>
2023-05-02 10:28:51 -0400

    Update the leaderboard service

--- ui/src/app/services/leaderboard.service.ts
+++ ui/src/app/services/leaderboard.service.ts
@@ -4,76 +4,123 @@ import { SessionService } from "./session.service";
 import { LoggerService } from "./logger.service";
 import { LeaderboardEntry } from "../types/other.types";
 
-/** Handles all operations related to fetching and calculating the leaderboard. */
+/** 
+ * A service that handles all operations related to fetching and
+ * calculating the leaderboard. 
+ * 
+ * @class LeaderboardService
+ * */
 @Injectable()
 export class LeaderboardService {
 
-  leaderboardGlobal: LeaderboardEntry[] = [];
-  leaderboardClass: LeaderboardEntry[] = [];
-  updatedOn: number = -1;
-  updatingGlobal = false;
-  updatingClass = false;
+  /** Whether or not the leaderboards need to be updated. */
+  get needsUpdate() { 
+    return Date.now() >= this._lastUpdatedOn + (15 * 60 * 1000)
+      && this._lastUpdatedOn !== -1;
+  }
+  set needsUpdate(_: boolean) { /** This property cannot be set. */ }
+
+  private _leaderboard: { [key: string]: LeaderboardEntry[] } = {
+    "global": [],
+    "class": []
+  }
+  /** 
+   * Returns a list of LeaderboardEntry objects based upon which
+   * leaderboard is indicated via a boolean.
+   * 
+   * @param global - True returns the global leaderboard;
+   * False returns the class leaderboard.
+   */
+  public leaderboard(global: boolean) {
+    if (this.needsUpdate) this.update();
+    return global 
+      ? this._leaderboard["global"] 
+      : this._leaderboard["class"];
+  }
+
+  // Internal property to track when the leaderboard was last updated.
+  private _lastUpdatedOn: number = -1;
+
+  // Internal properties to track if an update is currently in progress.
+  private _globalUpdateInProgress = false;
+  private _classUpdateInProgress = false;
 
   constructor(
     private _api: ApiService,
     private _session: SessionService,
     private _logger: LoggerService
   ) {
+    // Subscribes to the session's user observable to ensure that the
+    // leaderboard does not attempt to update until a session has
+    // properly begun.
     this._session.userSubject.subscribe((change) => {
-      if (change) {
-        this.update();
-      }
+      if (change) this.update();
     });
   }
 
-  log(func: string, message: string, meta?: any) {
+  /** Writes a log to the console as the leaderboard service. */
+  private log(func: string, message: string, meta?: any) {
     this._logger.log("leaderboard.service", func, message, meta);
   }
 
-  get needUpdate() {
-    return (
-      Date.now() >= this.updatedOn + (15 * 60 * 1000) 
-      && this.updatedOn !== -1
-    );
+  // Deprecated methods to allow unchanged segments to function.
+  /** @deprecated Prefer `leaderboard()` */
+  public GetGlobalLeaderboard(): LeaderboardEntry[] { 
+    return this.leaderboard(true); 
+  }
+  /** @deprecated Prefer `leaderboard()` */
+  public GetClassLeaderboard(): LeaderboardEntry[] { 
+    return this.leaderboard(false); 
   }
 
-  leaderboard(global: boolean) {
-    if (this.needUpdate) {
-      this.update();
-    }
-    return global ? this.leaderboardGlobal : this.leaderboardClass;
+  public update(): void {
+    this.log("update", "Updating the leaderboard ...");
+    this._lastUpdatedOn = Date.now();
+    this._updateGlobal();
+    this._updateClass();
   }
 
-  /** @deprecated Prefer `leaderboard()` */
-  GetGlobalLeaderboard() { return this.leaderboard(true); }
-  /** @deprecated Prefer `leaderboard()` */
-  GetClassLeaderboard() { return this.leaderboard(false); }
-
-  update() {
-    this.updatedOn = Date.now();
-    if (!this.updatingGlobal && this._session.isValid()) {
-      this.updatingGlobal = true;
-      this.leaderboardGlobal = [];
+  /** Updates the global leaderboard. */
+  private _updateGlobal(): void {
+    this.log("_updateGlobal", "Updating the global leaderboard ...");
+    if (!this._globalUpdateInProgress && this._session.isValid()) {
+      this._globalUpdateInProgress = true;
+      const leaderboardBuffer: LeaderboardEntry[] = [];
       this._api.getUsersByScore(5, (users) => {
         this._api.getUserScores(users, (score) => { 
-          this.leaderboardGlobal.push(score); 
-          if (this.leaderboardGlobal.length === users.length) { 
-            this.updatingGlobal = false; 
+          leaderboardBuffer.push(score); 
+          if (leaderboardBuffer.length === users.length) {
+            this.log(
+              "_updateGlobal", 
+              "Global leaderboard has finished its update."
+            );
+            this._leaderboard["global"] = leaderboardBuffer;
+            this._globalUpdateInProgress = false;
           }
         });
       });
     }
-    if (!this.updatingClass && this._session.isValid()) {
-      this.updatingClass = true;
-      this.leaderboardClass = [];
+  }
+
+  /** Updates the class leaderboard. */
+  private _updateClass(): void {
+    this.log("_updateClass", "Updating the class leaderboard ...");
+    if (!this._classUpdateInProgress && this._session.isValid()) {
+      this._classUpdateInProgress = true;
+      const leaderboardBuffer: LeaderboardEntry[] = [];
       this._api.getClassUsersByScore(
         this._session.user.validation_value, 
         5, 
         (users) => {
           this._api.getUserScores(users, (score) => { 
-            this.leaderboardClass.push(score); 
-            if (this.leaderboardClass.length === users.length) { 
-              this.updatingGlobal = false; 
+            leaderboardBuffer.push(score); 
+            if (leaderboardBuffer.length === users.length) { 
+              this.log(
+                "_updateClass", 
+                "Class leaderboard has finished its update."
+              );
+              this._leaderboard["class"] = leaderboardBuffer;
+              this._classUpdateInProgress = false; 
             }
           });
       });
