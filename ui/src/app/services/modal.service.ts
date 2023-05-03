bce2337a12f0e4f0577896eb5059074db3154dd7
Lockett, Charlotte F <lockettc@protonmail.com>
2023-05-03 13:04:30 -0400

    Fix formatting and docs for modal service

--- ui/src/app/services/modal.service.ts
+++ ui/src/app/services/modal.service.ts
@@ -15,12 +15,12 @@ export class ModalService {
   private _content: { [key: string]: typeof LoginModal } = {
     "login": LoginModal
   };
-  /** Dictionary of available content to display within a modal. */
+  /** 
+   * (Read-only)
+   * Dictionary of available content to display within a modal. 
+   */
   get content(): { [key: string]: typeof LoginModal } {
     return this._content;
-  }
-  set content(_: { [key: string]: typeof LoginModal }) {
-    /** This property cannot be set externally. */
   }
 
   // Internal reference to the current modal; possibly undefined.
@@ -32,12 +32,24 @@ export class ModalService {
     private _logger: LoggerService
   ) {}
 
-  /** Writes a log to the console from the ModalService. */
-  private log(func: string, message: string, meta?: any) {
+  /** 
+   * Writes a log to the console from the modal service.
+   * 
+   * @param func Function from which the log originates
+   * 
+   * @param message Content of the log
+   * 
+   * @param meta (Optional) Any additional data or information to log
+   */
+  private log(func: string, message: string, meta?: any): void {
     this._logger.log("modal.service", func, message, meta);
   }
 
-  /** Displays a modal containing the specified content. */
+  /** 
+   * Displays a modal containing the specified content. 
+   * 
+   * @param contentId Identifier for the desired content to display
+   */
   public show(contentId: string): void {
     if (!Object.keys(this._content).includes(contentId)) {
       return this.log(
@@ -57,9 +69,9 @@ export class ModalService {
   }
 
   /** Emits the signal to close the current modal. */
-  public close() {
+  public close(): void {
     if (this._modalRef)
-      this._modalRef.instance.closed.emit();
+      this._modalRef.instance.closed.next();
   }
 
   /** Creates a new modal instance and inserts it into the DOM. */
@@ -85,8 +97,12 @@ export class ModalService {
     document.body.appendChild(modal);
   }
 
-  /** Changes the content displayed within the current modal instance. */
-  private _changeContent(contentId: string) {
+  /** 
+   * Changes the content displayed within the current modal instance. 
+   * 
+   * @param contentId Identifier for the content to displasy
+   */
+  private _changeContent(contentId: string): void {
     if (!Object.keys(this._content).includes(contentId)) {
       return this.log(
         "changeContent", 
@@ -101,7 +117,7 @@ export class ModalService {
   }
 
   /** Detaches the current modal instance and clears it from the service. */
-  private _destroy() {
+  private _destroy(): void {
     if (this._modalRef) {
       this._app.detachView(this._modalRef.hostView);
       this._modalRef = undefined;
