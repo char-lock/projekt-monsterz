e04779055365e8f1e06028e51801f43ff1a7e2a1
Jesse Tortorella <72392491+Jtortorella@users.noreply.github.com>
2023-05-19 10:22:18 -0400

    Updated Comment

--- backend/src/endpoints/users/users.routes.ts
+++ backend/src/endpoints/users/users.routes.ts
@@ -19,7 +19,7 @@ router.get("/validation/:value", UsersController.getUsersByValidationValue);
 // Create a user
 router.post("/", UsersController.createUser);
 
-// Updates a user's score.
+// Updates a user's progress.
 router.post("/username/:username/progress", UsersController.postUserProgressByUsername);
 
 export default router;
